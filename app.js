a410cfc00d9eecaeb56f0f913daa5a50201b5f08
Phil <phil@htglearn.com>
2018-09-07 13:36:57 -0400

    Adding javascript code

---
+++ app.js
@@ -0,0 +1,6 @@
+// JavaScript file for JS and DOM integration project
+
+document.addEventListener('click',function(){
+    const mainHeading = document.querySelector('h1');
+    mainHeading.style.backgroundColor = 'orange';
+})
